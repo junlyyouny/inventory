43b32e72318c1cb04d89b1d8b9e0635282ba4d5b
junlyyouny <junlyyouny@gmail.com>
2017-02-03 14:39:08 +0800

    add alert zh-CN

--- resources/views/partials/errors.blade.php
+++ resources/views/partials/errors.blade.php
@@ -1,7 +1,8 @@
 @if (count($errors) > 0)
     <div class="alert alert-danger">
-        <strong>Whoops!</strong>
-        There were some problems with your input.<br><br>
+    	<button type="button" class="close" data-dismiss="alert">×</button>
+        <strong>糟糕!</strong>
+        您的输入有一些问题：<br><br>
         <ul>
         @foreach ($errors->all() as $error)
             <li>{{ $error }}</li>
--- resources/views/partials/success.blade.php
+++ resources/views/partials/success.blade.php
@@ -2,7 +2,7 @@
     <div class="alert alert-success">
         <button type="button" class="close" data-dismiss="alert">×</button>
         <strong>
-            <i class="fa fa-check-circle fa-lg fa-fw"></i> Success. 
+            <i class="fa fa-check-circle fa-lg fa-fw"></i> 操作成功： 
         </strong>
         {{ Session::get('success') }}
     </div>
